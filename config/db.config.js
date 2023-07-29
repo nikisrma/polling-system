bed7545a94492928aa301f6b2fc52f6b9131c32e
Nikita <nikita.sharma@devtechnosys.info>
2023-07-29 18:59:43 +0530

    node version

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,21 @@
 // Require the library
 const mongoose = require('mongoose');
+const username = process.env.DATABASE_USERNAME;
+const password =process.env.DATABASE_PASSWORD;
+const cluster =process.env.CLUSTER_NAME;
+const databaseName =process.env.DATABASE_NAME;
 
-// Connect to the local database
-mongoose.connect("mongodb://localhost/voting-system");
+const options = {
+  user: username,
+  pass: password,
+  dbName: databaseName,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: true,
+  w: 'majority',
+};
+mongoose.connect(`mongodb+srv://${cluster}.czmc1rn.mongodb.net/`, options);
+
 
 // Acquire the database
 const connection = mongoose.connection;
@@ -12,3 +25,4 @@ connection.on('error',console.error.bind(console,"error in database connection")
 
 // Successful connection
 connection.once('open',function(){console.log("Database connected successfully!")})
+
